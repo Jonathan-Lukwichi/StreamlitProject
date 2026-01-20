1faeeed6a23ce4562b4fb4cb72ca70d6a2c6f4ed
Jonathan-Lukwichi <jonathanlukwichi29@gmail.com>
2026-01-20 10:56:39 +0200

    Update: app_core\models\sarimax_pipeline.py [2026-01-20 10:56]

--- app_core/models/sarimax_pipeline.py
+++ app_core/models/sarimax_pipeline.py
@@ -299,9 +299,13 @@ def _auto_order_rmse_only(
 
     # Fit full model to get AIC/BIC for reporting
     try:
+        # Convert to numpy to avoid index alignment issues
+        y_tr_arr = y_tr.values
+        X_tr_arr = X_tr.values if X_tr is not None and X_tr.shape[1] > 0 else None
+
         full_model = SARIMAX(
-            endog=y_tr,
-            exog=X_tr if X_tr is not None and X_tr.shape[1] > 0 else None,
+            endog=y_tr_arr,
+            exog=X_tr_arr,
             order=tuple(best_row['order']),
             seasonal_order=tuple(best_row['seasonal_order']),
             enforce_stationarity=False,
